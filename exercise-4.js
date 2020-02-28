6e91f1e8b954160cb6830ea63d24b20f069f9e33
mubaraqhere <mubaraqhere@gmail.com>
2020-02-28 22:23:20 +0700

    add answer e-4

---
+++ exercise-4.js
@@ -0,0 +1,54 @@
+var tanggal = 30;
+var bulan = 2;
+var tahun = 2020;
+
+switch(bulan){
+    case 1:
+        bulan = "Januari"
+        break;
+    case 2:
+        bulan = "February"
+        break;
+    case 3:
+        bulan = "March"
+        break;
+    case 4:
+        bulan = "April"
+        break;
+    case 5:
+        bulan = "May"
+        break;
+    case 6:
+        bulan = "June"
+        break;
+    case 7:
+        bulan = "July"
+        break;
+    case 8:
+        bulan = "August"
+        break;
+    case 9:
+        bulan = "September"
+        break;
+    case 10:
+        bulan = "October"
+        break;
+    case 11:
+        bulan = "November"
+        break;
+    case 12:
+        bulan = "December"
+        break;
+}
+
+var calender = tanggal + " " + bulan + " " + tahun
+
+if (tanggal > 31 || tanggal < 1 ){
+    console.log("Masukkan tanggal yang sesuai!");
+} else if (bulan > 12 || bulan < 1){
+    console.log("Masukkan bulan yang sesuai!")
+} else if (tahun > 2200 || tahun < 1900){
+    console.log("Tahun hanya tersedia 1900 sampai 2200 saja :)")
+} else{
+    console.log(calender);
+}
